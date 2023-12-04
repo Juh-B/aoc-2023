d8fb75671bf8dfc88302a3cc25300d8bcf82e48b
Juliana Bonardi <julicbonardi@hotmail.com>
2023-12-04 04:34:55 -0300

    Part 02

--- Day_01/day01.rb
+++ Day_01/day01.rb
@@ -1,17 +1,57 @@
 class Day01
   file = File.readlines('./input.txt')
 
-  calibration = 0
+  # Part 01
+  part_01_calibration = 0
+
+  file.each do |line|
+    numbers = []
+
+    numbers << line.scan(/\d/).first
+    numbers << line.scan(/\d/).last
+
+    part_01_calibration += numbers.join.to_i
+  end
+
+  # Part 02
+  part_02_calibration = 0
 
   file.each do |line|
     number = []
+    words = {
+      "one" => 1,
+      "two" => 2,
+      "three" => 3,
+      "four" => 4,
+      "five" => 5,
+      "six" => 6,
+      "seven" => 7,
+      "eight" => 8,
+      "nine" => 9,
+      "1" => 1,
+      "2" => 2,
+      "3" => 3,
+      "4" => 4,
+      "5" => 5,
+      "6" => 6,
+      "7" => 7,
+      "8" => 8,
+      "9" => 9
+    }
 
-    number << line.scan(/\d/).first
-    number << line.scan(/\d/).last
+    first_digit = line.scan(/\d|one|two|three|four|five|six|seven|eight|nine/).first
+    last_digit = line.scan(/\d|one|two|three|four|five|six|seven|eight|nine/).last
 
-    calibration += number.join.to_i
+    number << words[first_digit]
+    number << words[last_digit]
+    part_02_calibration += number.join.to_i
+
   end
 
-  puts "Part 01 -> The sum of the calibration values is #{calibration}."
-  puts '------------'
+  puts part_01_calibration
+  puts part_02_calibration
+
+  # puts "Part 01 -> The sum of the calibration values is #{part_01_calibration}."
+  # puts '------------'
+  # puts "Part 02 -> The sum of the calibration values is #{part_02_calibration}."
 end
