98cf92607ec2de40459d9e4d92b03152e079246a
Juliana Bonardi <julicbonardi@hotmail.com>
2023-12-05 01:40:50 -0300

    Fixing and Solving problems in part 02

--- Day_01/day01.rb
+++ Day_01/day01.rb
@@ -1,7 +1,7 @@
 class Day01
   file = File.readlines('./input.txt')
 
-  # Part 01
+  # # Part 01
   part_01_calibration = 0
 
   file.each do |line|
@@ -13,22 +13,25 @@ class Day01
     part_01_calibration += numbers.join.to_i
   end
 
-  # Part 02
+  # # Part 02
   part_02_calibration = 0
 
   file.each do |line|
     number = []
     words = {
-      'one' => 1, 'two' => 2, 'three' => 3, 'four' => 4, 'five' => 5,
-      'six' => 6, 'seven' => 7, 'eight' => 8, 'nine' => 9,
-      '1' => 1, '2' => 2, '3' => 3, '4' => 4, '5' => 5,
-      '6' => 6, '7' => 7, '8' => 8, '9' => 9
+      'one' => '1', 'two' => '2', 'three' => '3', 'four' => '4', 'five' => '5',
+      'six' => '6', 'seven' => '7', 'eight' => '8', 'nine' => '9',
+      'eno' => '1', 'owt' => '2', 'eerht' => '3', 'ruof' => '4', 'evif' => '5',
+      'xis' => '6', 'neves' => '7', 'thgie' => '8', 'enin' => '9',
+      '1' => '1', '2' => '2', '3' => '3', '4' => '4', '5' => '5',
+      '6' => '6', '7' => '7', '8' => '8', '9' => '9', '0' => '0'
     }
 
-    digits = line.scan(/\d|one|two|three|four|five|six|seven|eight|nine/)
+    first_digits = line.scan(/\d|one|two|three|four|five|six|seven|eight|nine/)
+    last_digits = line.reverse.scan(/\d|eno|owt|eerht|ruof|evif|xis|neves|thgie|enin/)
 
-    number << words[digits.first]
-    number << words[digits.last]
+    number << words[first_digits[0]]
+    number << words[last_digits[0]]
 
     part_02_calibration += number.join.to_i
   end
