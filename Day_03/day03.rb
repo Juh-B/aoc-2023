726637588276a9607f7045cf2fc7e8124673c969
Juliana Bonardi <julicbonardi@hotmail.com>
2023-12-15 16:41:31 -0300

    Part 02

--- Day_03/day03.rb
+++ Day_03/day03.rb
@@ -1,19 +1,62 @@
 require 'set'
 class Day03
-  file = File.readlines('./input.txt')
+  file = File.readlines('./sample.txt')
 
   # Part 01
-  syms = []
+  # syms = []
+  # file.each_with_index do |row, x|
+  #   row.chomp.each_char.with_index do |char, y|
+  #     next if char == "."
+  #     next if char =~ /\d/
+  #     syms << [x, y]
+  #   end
+  # end
+
+  # number_locations =[]
+  # syms.each do |(x, y)|
+  #   [
+  #     [-1,-1],
+  #     [-1, 0],
+  #     [-1, 1],
+  #     [0, -1],
+  #     [0, 1],
+  #     [1, -1],
+  #     [1, 0],
+  #     [1, 1],
+  #   ].each do |(dx, dy)|
+  #     nx = x + dx
+  #     ny = y + dy
+  #     if file[nx][ny] =~ /\d/
+  #       number_locations << [nx, ny]
+  #     end
+  #   end
+  # end
+
+  # number_starts = Set.new
+  # number_locations.each do |(x, y)|
+  #   while file[x][y - 1] =~ /\d/
+  #     y -= 1
+  #   end
+  #   number_starts << [x, y]
+  # end
+
+  # result = number_starts.map do |(x, y)|
+  #   file[x][y..].to_i
+  # end
+
+  # p result.sum
+
+  # Part 02
+  syms_02 = []
   file.each_with_index do |row, x|
     row.chomp.each_char.with_index do |char, y|
-      next if char == "."
-      next if char =~ /\d/
-      syms << [x, y]
+        syms_02 << [x, y] if char == "*"
     end
   end
 
-  number_locations =[]
-  syms.each do |(x, y)|
+  gear_ratios = []
+  syms_02.each do |(x, y)|
+    number_locations =[]
     [
       [-1,-1],
       [-1, 0],
@@ -30,19 +73,22 @@ class Day03
         number_locations << [nx, ny]
       end
     end
-  end
 
-  number_starts = Set.new
-  number_locations.each do |(x, y)|
-    while file[x][y - 1] =~ /\d/
-      y -= 1
+    number_starts = Set.new
+    number_locations.each do |(x, y)|
+      while file[x][y - 1] =~ /\d/
+        y -= 1
+      end
+      number_starts << [x, y]
     end
-    number_starts << [x, y]
+
+    if number_starts.length == 2
+      result = number_starts.map do |(x, y)|
+        file[x][y..].to_i
+      end.inject(:*)
+      gear_ratios << result
+    end
   end
 
-  result = number_starts.map do |(x, y)|
-    file[x][y..].to_i
-  end
-
-  p result.sum
+  p gear_ratios.sum
 end
