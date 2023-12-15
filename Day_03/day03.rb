53475d7adff49af23e44996282daf04236a818ed
Juliana Bonardi <julicbonardi@hotmail.com>
2023-12-15 16:49:03 -0300

    Part 02 Complete

--- Day_03/day03.rb
+++ Day_03/day03.rb
@@ -1,50 +1,52 @@
 require 'set'
 class Day03
-  file = File.readlines('./sample.txt')
+  file = File.readlines('./input.txt')
 
   # Part 01
-  # syms = []
-  # file.each_with_index do |row, x|
-  #   row.chomp.each_char.with_index do |char, y|
-  #     next if char == "."
-  #     next if char =~ /\d/
-  #     syms << [x, y]
-  #   end
-  # end
+  syms = []
+  file.each_with_index do |row, x|
+    row.chomp.each_char.with_index do |char, y|
+      next if char == "."
+      next if char =~ /\d/
+      syms << [x, y]
+    end
+  end
 
-  # number_locations =[]
-  # syms.each do |(x, y)|
-  #   [
-  #     [-1,-1],
-  #     [-1, 0],
-  #     [-1, 1],
-  #     [0, -1],
-  #     [0, 1],
-  #     [1, -1],
-  #     [1, 0],
-  #     [1, 1],
-  #   ].each do |(dx, dy)|
-  #     nx = x + dx
-  #     ny = y + dy
-  #     if file[nx][ny] =~ /\d/
-  #       number_locations << [nx, ny]
-  #     end
-  #   end
-  # end
+  number_locations =[]
+  syms.each do |(x, y)|
+    [
+      [-1,-1],
+      [-1, 0],
+      [-1, 1],
+      [0, -1],
+      [0, 1],
+      [1, -1],
+      [1, 0],
+      [1, 1],
+    ].each do |(dx, dy)|
+      nx = x + dx
+      ny = y + dy
+      if file[nx][ny] =~ /\d/
+        number_locations << [nx, ny]
+      end
+    end
+  end
 
-  # number_starts = Set.new
-  # number_locations.each do |(x, y)|
-  #   while file[x][y - 1] =~ /\d/
-  #     y -= 1
-  #   end
-  #   number_starts << [x, y]
-  # end
+  number_starts = Set.new
+  number_locations.each do |(x, y)|
+    while file[x][y - 1] =~ /\d/
+      y -= 1
+    end
+    number_starts << [x, y]
+  end
 
-  # result = number_starts.map do |(x, y)|
-  #   file[x][y..].to_i
-  # end
+  result = number_starts.map do |(x, y)|
+    file[x][y..].to_i
+  end
 
-  # p result.sum
+  puts "Part 01 -> The sum of the part numbers in the engine schematic is #{result.sum}."
+  puts '------------'
+
 
   # Part 02
   syms_02 = []
@@ -90,5 +92,5 @@ class Day03
     end
   end
 
-  p gear_ratios.sum
+  puts "Part 02 -> The sum of all of the gear ratios is #{gear_ratios.sum}."
 end
